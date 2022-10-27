442408fa35ff92bee1203cc1af521d9b614a9964
daogilvie <drum.ogilvie@ovo.com>
2022-10-27 11:01:20 +0100

    nvim: Try inverted treesitter class brackets

--- .config/nvim/plugin/treesitter.lua
+++ .config/nvim/plugin/treesitter.lua
@@ -34,11 +34,11 @@ require 'nvim-treesitter.configs'.setup {
       set_jumps = true, -- whether to set jumps in the jumplist
       goto_next_start = {
         ["]m"] = "@function.outer",
-        ["]]"] = "@class.outer",
+        ["]["] = "@class.outer",
       },
       goto_next_end = {
         ["]M"] = "@function.outer",
-        ["]["] = "@class.outer",
+        ["]]"] = "@class.outer",
       },
       goto_previous_start = {
         ["[m"] = "@function.outer",
