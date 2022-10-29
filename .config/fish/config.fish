70233478e57d7909dd1737dfd33c8c77a0a741f3
Drum Ogilvie <me@daogilvie.com>
2022-10-29 18:30:51 +0100

    fish: turn off more things for warp

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,7 @@
-if test $TERM_PROGRAM = "iTerm.app" 
+if test "$TERM_PROGRAM" = "iTerm.app"
     test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+    set -x ITERM_ENABLE_SHELL_INTEGRATION_WITH_TMUX YES
 end
-set -x ITERM_ENABLE_SHELL_INTEGRATION_WITH_TMUX YES
 
 # Set nvim as editor
 set -g -x EDITOR nvim
@@ -10,5 +10,7 @@ set -g -x VISUAL nvim
 # And less as pager
 set -g -x PAGER less
 
-# Enable VI mode bindings
-fish_vi_key_bindings
+if test "$TERM_PROGRAM" != "WarpTerminal"
+    # Enable VI mode bindings
+    fish_vi_key_bindings
+end
