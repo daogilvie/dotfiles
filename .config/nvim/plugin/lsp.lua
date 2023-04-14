5f7bdd5cfeaa03be93fb77ebb2360bd505e272d8
Drum Ogilvie <me@daogilvie.com>
2023-04-14 08:42:58 +0100

    nvim: Fix mason-null-lsp setup change

--- .config/nvim/plugin/lsp.lua
+++ .config/nvim/plugin/lsp.lua
@@ -214,6 +214,6 @@ require("mason-null-ls").setup({
     },
     automatic_installation = false,
     automatic_setup = true, -- Recommended, but optional
+    handlers = {},
 })
 require("null-ls").setup()
-require('mason-null-ls').setup_handlers()
