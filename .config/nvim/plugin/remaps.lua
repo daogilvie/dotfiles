25e0d159688a91e8c10cd44fa10d0f8fa4c0abcb
Drum Ogilvie <me@daogilvie.com>
2023-06-19 09:18:45 +0100

    nvim: format remaps, find_files includes hidden

--- .config/nvim/plugin/remaps.lua
+++ .config/nvim/plugin/remaps.lua
@@ -1,5 +1,5 @@
 local function map(modes, shortcut, command_or_fn)
-  vim.keymap.set(modes, shortcut, command_or_fn, { noremap = true, silent = true })
+    vim.keymap.set(modes, shortcut, command_or_fn, { noremap = true, silent = true })
 end
 
 -- Convenient saving
@@ -24,25 +24,29 @@ map({ 'n', 'v' }, '<leader>d', '"_d')
 
 local tele = require 'telescope.builtin'
 local tele_file_browser = require 'telescope'.extensions.file_browser.file_browser
+local function tele_find_with_hidden()
+    local opts = { hidden = true }
+    return tele.find_files(opts)
+end
 
 map({ 'n' }, '<C-f>', tele.live_grep)
-map({ 'n' }, '<C-p>', tele.find_files)
+map({ 'n' }, '<C-p>', tele_find_with_hidden)
 map({ 'n' }, '<leader>e', tele.symbols)
 map({ 'n' }, '<leader>p', tele.buffers)
 map({ 'n' }, '<leader>o', tele_file_browser)
 map({ 'n' }, '<leader>O', function()
-  tele_file_browser({ path = '%:p:h', cwd_to_path = true })
+    tele_file_browser({ path = '%:p:h', cwd_to_path = true })
 end)
 map({ 'n' }, '<leader>sw', function()
-  tele.grep_string({ search = vim.fn.expand('<cword>') })
+    tele.grep_string({ search = vim.fn.expand('<cword>') })
 end)
 map({ 'n' }, '<leader>ss', function()
-  tele.grep_string({ search = vim.fn.input('Rg> '), use_regex = true })
+    tele.grep_string({ search = vim.fn.input('Rg> '), use_regex = true })
 end)
 map({ 'n' }, '<leader>R', tele.lsp_dynamic_workspace_symbols)
 map({ 'n' }, '<leader>r', tele.lsp_document_symbols)
 map({ 'n' }, '<leader>rw', function()
-  tele.lsp_workspace_symbols({ query = vim.fn.expand('<cword>') })
+    tele.lsp_workspace_symbols({ query = vim.fn.expand('<cword>') })
 end)
 
 -- Various git-related things
@@ -57,7 +61,7 @@ map({ 'n' }, 'gdl', ':diffget //3<CR>')
 -- Test stuff
 local neotest = require('neotest')
 local function test_current_file()
-  return neotest.run.run(vim.fn.expand("%"))
+    return neotest.run.run(vim.fn.expand("%"))
 end
 
 map({ 'n' }, '<leader>ts', neotest.summary.toggle)
