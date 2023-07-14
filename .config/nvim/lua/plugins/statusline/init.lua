86f1d70aa4061612ceb5933a66b5f680d7f4dc41
Drum Ogilvie <me@daogilvie.com>
2023-07-14 12:49:24 +0100

    tidy up status and winbar

--- .config/nvim/lua/plugins/statusline/init.lua
+++ .config/nvim/lua/plugins/statusline/init.lua
@@ -5,7 +5,7 @@ local function code_location()
         local location = navic.get_location()
         local nice_location = ""
         if location ~= nil and location ~= "" then
-            nice_location = "%#WinBarContext#" .. " " .. icons.ui.ChevronRight .. " " .. location .. "%*"
+            nice_location = "%#WinBarContext#" .. icons.ui.ChevronRight .. " " .. location .. " %*"
         end
         return nice_location
     else
@@ -41,43 +41,36 @@ return {
                 },
                 sections = {
                     lualine_a = { { "fancy_mode", width = 3 } },
-                    lualine_b = { components.git_repo, "branch" },
+                    lualine_b = {
+                        { "fancy_cwd", substitute_home = true },
+                        components.git_repo, "branch" },
                     lualine_c = {
-                        "filename",
-                        { "fancy_cwd",        substitute_home = true },
-                        components.diff,
-                        { "fancy_diagnostics" },
-                        components.separator,
-                        components.lsp_client,
+                        "fancy_lsp_servers"
                     },
-                    lualine_x = { components.spaces, "encoding", "fileformat", "filetype", "progress" },
-                    lualine_y = {},
-                    lualine_z = { "location" },
-                },
-                inactive_sections = {
-                    lualine_a = {},
-                    lualine_b = {},
-                    lualine_c = { "filename" },
-                    lualine_x = { "location" },
+                    lualine_x = { components.spaces, "encoding", },
                     lualine_y = {},
                     lualine_z = {},
                 },
                 extensions = { "toggleterm", "quickfix" },
                 winbar = {
-                    lualine_a = { "filename", code_location },
+                    lualine_a = { { "filename", path = 1 }, code_location },
                     lualine_b = {},
                     lualine_c = {},
-                    lualine_x = {},
-                    lualine_y = {},
-                    lualine_z = {},
+                    lualine_x = { components.diagnostics },
+                    lualine_y = {
+                        components.diff,
+                    },
+                    lualine_z = { "filetype", },
                 },
                 inactive_winbar = {
-                    lualine_a = { "filename" },
+                    lualine_a = { { "filename", path = 1 }, },
                     lualine_b = {},
                     lualine_c = {},
-                    lualine_x = {},
-                    lualine_y = {},
-                    lualine_z = {},
+                    lualine_x = { components.diagnostics },
+                    lualine_y = {
+                        components.diff,
+                    },
+                    lualine_z = { "filetype" },
                 },
             }
         end,
