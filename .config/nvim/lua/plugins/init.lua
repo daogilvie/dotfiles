c1b7ba5ec9a5ea68ea393dadae6f639dce5868af
Drum Ogilvie <me@daogilvie.com>
2024-06-19 11:22:56 +0100

    nvim: Fix overseer templates, add git ones

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -136,8 +136,9 @@ return {
             { "<leader>rs", "<cmd>OverseerSaveBundle<cr>",   desc = "Save Bundle" },
             { "<leader>rt", "<cmd>OverseerToggle<cr>",       desc = "Toggle" },
         },
+        lazy = false,
         opts = {
-          templates = {"builtin"}
+            templates = { "builtin", "zig", "git" }
         },
         config = function(_, opts)
             local overseer = require('overseer')
