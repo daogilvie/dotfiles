ec0c6566502dea221cf5c4e8948a1fa75d224650
Drum Ogilvie <me@daogilvie.com>
2024-07-06 12:24:46 +0100

    nvim: format some lua files

--- .config/nvim/lua/plugins/vcs.lua
+++ .config/nvim/lua/plugins/vcs.lua
@@ -1,99 +1,114 @@
 -- Taken largely from alpha2phi again
 return {
-    {
-        "sindrets/diffview.nvim",
-        cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles" },
-        config = true,
+  {
+    "sindrets/diffview.nvim",
+    cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles" },
+    config = true,
+  },
+  {
+    "tpope/vim-fugitive",
+    cmd = { "Git", "GBrowse", "Gdiffsplit", "Gvdiffsplit" },
+    dependencies = {
+      "tpope/vim-rhubarb",
+      "tyru/open-browser.vim"
     },
-    {
-        "tpope/vim-fugitive",
-        cmd = { "Git", "GBrowse", "Gdiffsplit", "Gvdiffsplit" },
-        dependencies = {
-            "tpope/vim-rhubarb",
-        },
-        keys = {
-            { "<leader>gg", ":Git <CR>", desc = "Open Fugitive" },
-        }
+    keys = {
+      { "<leader>gg", ":Git <CR>", desc = "Open Fugitive" },
     },
-    {
-        "lewis6991/gitsigns.nvim",
-        event = "BufReadPre",
-        opts = {
-            signs = {
-                add = { hl = "GitSignsAdd", text = "▍", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-                change = {
-                    hl = "GitSignsChange",
-                    text = "▍",
-                    numhl = "GitSignsChangeNr",
-                    linehl = "GitSignsChangeLn",
-                },
-                delete = {
-                    hl = "GitSignsDelete",
-                    text = "▸",
-                    numhl = "GitSignsDeleteNr",
-                    linehl = "GitSignsDeleteLn",
-                },
-                topdelete = {
-                    hl = "GitSignsDelete",
-                    text = "▾",
-                    numhl = "GitSignsDeleteNr",
-                    linehl = "GitSignsDeleteLn",
-                },
-                changedelete = {
-                    hl = "GitSignsChange",
-                    text = "▍",
-                    numhl = "GitSignsChangeNr",
-                    linehl = "GitSignsChangeLn",
-                },
-            },
-            on_attach = function(bufnr)
-                local gs = package.loaded.gitsigns
-                local function map(mode, l, r, opts)
-                    opts = opts or {}
-                    opts.buffer = bufnr
-                    vim.keymap.set(mode, l, r, opts)
-                end
+    config = function()
+      vim.api.nvim_create_user_command("Browse", "OpenBrowser <args>", { nargs = 1 })
+    end
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    event = "BufReadPre",
+    opts = {
+      signs                        = {
+        add          = { text = '┃' },
+        change       = { text = '┃' },
+        delete       = { text = '_' },
+        topdelete    = { text = '‾' },
+        changedelete = { text = '~' },
+        untracked    = { text = '┆' },
+      },
+      signcolumn                   = true,  -- Toggle with `:Gitsigns toggle_signs`
+      numhl                        = false, -- Toggle with `:Gitsigns toggle_numhl`
+      linehl                       = false, -- Toggle with `:Gitsigns toggle_linehl`
+      word_diff                    = false, -- Toggle with `:Gitsigns toggle_word_diff`
+      watch_gitdir                 = {
+        follow_files = true
+      },
+      auto_attach                  = true,
+      attach_to_untracked          = false,
+      current_line_blame           = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+      current_line_blame_opts      = {
+        virt_text = true,
+        virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+        delay = 1000,
+        ignore_whitespace = false,
+        virt_text_priority = 100,
+      },
+      current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
+      sign_priority                = 6,
+      update_debounce              = 100,
+      status_formatter             = nil,   -- Use default
+      max_file_length              = 40000, -- Disable if file is longer than this (in lines)
+      preview_config               = {
+        -- Options passed to nvim_open_win
+        border = 'single',
+        style = 'minimal',
+        relative = 'cursor',
+        row = 0,
+        col = 1
+      },
+      on_attach                    = function(bufnr)
+        local gs = package.loaded.gitsigns
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
 
-                map("n", "]c", function()
-                    if vim.wo.diff then
-                        return "]c"
-                    end
-                    vim.schedule(function()
-                        gs.next_hunk()
-                    end)
-                    return "<Ignore>"
-                end, { expr = true })
+        map("n", "]c", function()
+          if vim.wo.diff then
+            return "]c"
+          end
+          vim.schedule(function()
+            gs.next_hunk()
+          end)
+          return "<Ignore>"
+        end, { expr = true })
 
-                map("n", "[c", function()
-                    if vim.wo.diff then
-                        return "[c"
-                    end
-                    vim.schedule(function()
-                        gs.prev_hunk()
-                    end)
-                    return "<Ignore>"
-                end, { expr = true })
+        map("n", "[c", function()
+          if vim.wo.diff then
+            return "[c"
+          end
+          vim.schedule(function()
+            gs.prev_hunk()
+          end)
+          return "<Ignore>"
+        end, { expr = true })
 
-                -- Actions
-                map({ "n", "v" }, "<leader>ghs", ":Gitsigns stage_hunk<CR>", { desc = "Stage Hunk" })
-                map({ "n", "v" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>", { desc = "Reset Hunk" })
-                map("n", "<leader>ghS", gs.stage_buffer, { desc = "Stage Buffer" })
-                map("n", "<leader>ghu", gs.undo_stage_hunk, { desc = "Undo Stage Hunk" })
-                map("n", "<leader>ghR", gs.reset_buffer, { desc = "Reset Buffer" })
-                map("n", "<leader>ghp", gs.preview_hunk, { desc = "Preview Hunk" })
-                map("n", "<leader>ghb", function()
-                    gs.blame_line { full = true }
-                end, { desc = "Blame Line" })
-                map("n", "<leader>gtb", gs.toggle_current_line_blame, { desc = "Toggle Line Blame" })
-                map("n", "<leader>ghd", gs.diffthis, { desc = "Diff This" })
-                map("n", "<leader>ghD", function()
-                    gs.diffthis "~"
-                end, { desc = "Diff This ~" })
-                map("n", "<leader>gtd", gs.toggle_deleted, { desc = "Toggle Delete" })
+        -- Actions
+        map({ "n", "v" }, "<leader>ghs", ":Gitsigns stage_hunk<CR>", { desc = "Stage Hunk" })
+        map({ "n", "v" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>", { desc = "Reset Hunk" })
+        map("n", "<leader>ghS", gs.stage_buffer, { desc = "Stage Buffer" })
+        map("n", "<leader>ghu", gs.undo_stage_hunk, { desc = "Undo Stage Hunk" })
+        map("n", "<leader>ghR", gs.reset_buffer, { desc = "Reset Buffer" })
+        map("n", "<leader>ghp", gs.preview_hunk, { desc = "Preview Hunk" })
+        map("n", "<leader>ghb", function()
+          gs.blame_line { full = true }
+        end, { desc = "Blame Line" })
+        map("n", "<leader>gtb", gs.toggle_current_line_blame, { desc = "Toggle Line Blame" })
+        map("n", "<leader>ghd", gs.diffthis, { desc = "Diff This" })
+        map("n", "<leader>ghD", function()
+          gs.diffthis "~"
+        end, { desc = "Diff This ~" })
+        map("n", "<leader>gtd", gs.toggle_deleted, { desc = "Toggle Delete" })
 
-                -- Text object
-                map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", { desc = "Select Hunk" })
-            end,
-        },
+        -- Text object
+        map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", { desc = "Select Hunk" })
+      end,
     },
+  },
 }
